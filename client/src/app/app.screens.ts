4794c0caf23058f86cd8a322a00ad5a90ba1137a
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-26 16:54:57 +0300

    [Studio] Add screen "Blank"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,19 +2,14 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
-import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    Blank_PhonePortrait,
-    Login_PhonePortrait
+    Blank_PhonePortrait
   ];
   static mapping = {
     'Blank': {
       PhonePortrait: Blank_PhonePortrait
-    },
-    'Login': {
-      PhonePortrait: Login_PhonePortrait
     }
   }
 }
