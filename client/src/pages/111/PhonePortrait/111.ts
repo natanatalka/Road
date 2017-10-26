8947ad336398de0d5fecf8aeb0531b63b29cbe24
test <test@test.com>
2017-10-26 17:37:57 +0300

    [Studio] Add screen "111"

---
+++ client/src/pages/111/PhonePortrait/111.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-111-phoneportrait',
+  templateUrl: '111.html'
+})
+export class 111_PhonePortrait extends Screen {
+}
